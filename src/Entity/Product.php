f729eed70eda0358495ee2679a4b2092d828d0cb
jhon <jhon@ubuntuServer.home>
2018-11-04 23:51:57 +0100

    Add User Entity

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -5,7 +5,7 @@ namespace App\Entity;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\Entity(repositoryClass="ProductRepository")
  */
 class Product
 {
