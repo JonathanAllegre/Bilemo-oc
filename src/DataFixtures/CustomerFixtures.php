5dc6b258cd2a86e4f4756fe15f3975ae9290bedd
jhon <jhon@ubuntuServer.home>
2018-11-05 22:24:10 +0100

    Add Customer FIxtures

---
+++ src/DataFixtures/CustomerFixtures.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Customer;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class CustomerFixtures extends Fixture
+{
+
+    private $encoder;
+    private $manager;
+
+    const CUSTOMER_ONE_REFERENCE   = "Client 1";
+    const CUSTOMER_TWO_REFERENCE   = "Client 2";
+    const CUSTOMER_THREE_REFERENCE = "Client 3";
+    const CUSTOMER_FOUR_REFERENCE  = "Client 4";
+
+    public function __construct(UserPasswordEncoderInterface $encoder, ObjectManager $manager)
+    {
+        $this->encoder = $encoder;
+        $this->manager = $manager;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+
+        $customer1 = $this->newCustomer('Client 1', 'admin');
+        $this->addReference(self::CUSTOMER_ONE_REFERENCE, $customer1);
+
+        $customer2 = $this->newCustomer('Client 2', 'admin');
+        $this->addReference(self::CUSTOMER_TWO_REFERENCE, $customer2);
+
+        $customer3 = $this->newCustomer('Client 3', 'admin');
+        $this->addReference(self::CUSTOMER_THREE_REFERENCE, $customer3);
+
+        $customer4 = $this->newCustomer('Client 4', 'admin');
+        $this->addReference(self::CUSTOMER_FOUR_REFERENCE, $customer4);
+
+        $manager->flush();
+    }
+
+    public function newCustomer(string $name, string $password)
+    {
+        $customer = new Customer();
+        $customer->setName($name);
+        $customer->setPassword($this->encoder->encodePassword($customer, $password));
+
+        $this->manager->persist($customer);
+
+        return $customer;
+    }
+}
